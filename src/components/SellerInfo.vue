ab9b4567efb91773278e7b768012031f8fb42a26
CZXHenry <812582916@qq.com>
2018-06-06 00:00:01 +0800

    Add SellerInfo.vue

--- src/components/SellerInfo.vue
+++ src/components/SellerInfo.vue
@@ -1,21 +1,36 @@
 <template>
   <div class="Info">
-    <mt-button type="primary" @click="showToast">click</mt-button>
+  <mt-swipe ref="swipe" class="swipe" :auto="1000">
+    <mt-swipe-item v-for="img in imgs" :key="img.id">
+      <img  :src="img.url" alt="logo" :width="width" height="width"/>
+    </mt-swipe-item>
+  </mt-swipe>
   </div>
 </template>
 
 <script>
-import {Toast} from 'mint-ui'
-import {Swipe,SwipeItem} from 'mint-ui'
+import Vue from 'vue'
+import { Swipe, SwipeItem } from 'mint-ui'
 
-Vue.component(Swipe.name,Swipe)
-
+Vue.component(Swipe.name, Swipe)
+Vue.component(SwipeItem.name, SwipeItem)
 
 export default {
-  methods: {
-    showToast () {
-      Toast('toast')
-    }
+  props: {
+    imgs: [
+      { url: 'https://github.com/CourseCloudDesktop/cloudDesktop/raw/wcl-develop/task5/images/1.PNG' },
+      { url: 'https://github.com/CourseCloudDesktop/cloudDesktop/raw/wcl-develop/task5/images/2.PNG' },
+      { url: 'https://github.com/CourseCloudDesktop/cloudDesktop/raw/wcl-develop/task5/images/3.PNG' }
+    ],
+    seen: true
   }
 }
 </script>
+
+<style>
+.mint-swipe{
+  background: red;
+  height: 100px;
+  margin-top: 0px;
+}
+</style>
